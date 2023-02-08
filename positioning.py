8cbb1597e4d0583321b59770c3edbba718d10667
tinpott <tinpott>
2023-02-08 18:45:58 -0500

    Add function to calculate Pythagorean theorem

--- positioning.py
+++ positioning.py
@@ -8,6 +8,12 @@ class ObservationPoint:
 		self.x = x
 		self.y = y
 
+# Takes two 2D coords and returns the distance between them
+def pythag(a:tuple[float, float], b:tuple[float, float]) -> float:
+	dx = a[0] - b[0]
+	dy = a[1] - b[1]
+	return math.sqrt(dx**2 + dy**2)
+
 def main():
 	# TODO: Implement main function
 	pass
