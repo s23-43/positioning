e7107dbb7ef5b5ddf798ff546e7287616d7bf965
tinpott <tinpott>
2023-02-08 18:45:14 -0500

    Start class for OPs

---
+++ positioning.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+
+# Observation points have a 2D coordinate to represent position
+class ObservationPoint:
+	def __init__(self, x:float, y:float):
+		self.x = x
+		self.y = y
+
+def main():
+	# TODO: Implement main function
+	pass
+
+if __name__ == "__main__":
+	main()
