a1d8e362c4f099bcff9a0000c78386b9a579c1bf
tinpott <77856809+tinpott@users.noreply.github.com>
2023-03-16 16:29:20 -0400

    Add preliminary tests for Friis transmission formula

--- friis.py
+++ friis.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import numpy
 import math
 import sys
+
+def calculate_power_rx(power_tx: float, gain_tx: float, gain_rx: float, wavelength: float, distance: float) -> float:
+	power_rx = power_tx + gain_tx + gain_rx + 20*math.log10(wavelength / (4 * math.pi * distance))
+	return power_rx
+
+def calculate_power_rx_log_normal(power_tx: float, gain_tx: float, gain_rx: float, wavelength: float, distance: float, standard_deviation) -> float:
+	rand = numpy.random.normal(0, standard_deviation)
+	power_rx = calculate_power_rx(power_tx, gain_tx, gain_rx, wavelength, distance)
+	return power_rx + rand
 
 def calculate_distance(power_rx: float, power_tx: float, gain_rx: float, gain_tx: float, wavelength: float) -> float:
 	"""
@@ -28,11 +38,11 @@ def calculate_distance(power_rx: float, power_tx: float, gain_rx: float, gain_tx
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-pr", type=float, required=True, help="Signal power of Rx (dBm)")
-	parser.add_argument("-pt", type=float, required=True, help="Signal power of Tx (dBm)")
-	parser.add_argument("-gr", type=float, required=True, help="Antenna gain of Rx (dBi)")
-	parser.add_argument("-gt", type=float, required=True, help="Antenna gain of Tx (dBi)")
-	parser.add_argument("-wl", type=float, required=True, help="Signal wavelength. Output distance will be the same distance units as this")
+	parser.add_argument("-pr", "--power-rx", type=float, required=True, help="Signal power of Rx (dBm)")
+	parser.add_argument("-pt", "--power-tx", type=float, required=True, help="Signal power of Tx (dBm)")
+	parser.add_argument("-gr", "--gain-rx", type=float, required=True, help="Antenna gain of Rx (dBi)")
+	parser.add_argument("-gt", "--gain-tx", type=float, required=True, help="Antenna gain of Tx (dBi)")
+	parser.add_argument("-wl", "--power-rx", type=float, required=True, help="Signal wavelength. Output distance will be the same distance units as this")
 	args = parser.parse_args()
 
 	distance: float = calculate_distance(args.pr, args.pt, args.gr, args.gt, args.wl)
--- test.py
+++ test.py
@@ -4,7 +4,28 @@ import friis
 import positioning
 
 def friis_tests() -> None:
-	pass
+	xc,yc = (4, 4)
+	wavelength = 0.1
+	xCoords = [ 0.00, 3.00, 10.00 ]
+	yCoords = [ 0.00, 8.00,  5.00 ]
+	power_tx = 0
+	gain_tx = 0
+	gain_rx = [0, 0, 0]
+	distances = list()
+	for x, y in zip(xCoords, yCoords):
+		distances.append(positioning.pythag((x,y), (xc, yc)))
+	powers_rx = list()
+	for i in range(len(distances)):
+		powers_rx.append(friis.calculate_power_rx_log_normal(power_tx, gain_tx, gain_rx[i], wavelength, distances[i], 5))
+
+	calculated_distances = list()
+	for i in range(len(powers_rx)):
+		calculated_distances.append(friis.calculate_distance(powers_rx[i], power_tx, gain_rx[i], gain_tx, wavelength))
+	print(calculated_distances)
+
+	roots = positioning.calculate_roots(len(calculated_distances), calculated_distances, xCoords, yCoords)
+	print((xc, yc))
+	print(positioning.estimate_position(roots))
 
 def pos_tests() -> None:
 	radii   = [ 5.66, 4.12,  6.08, 4.24, 4.24 ]
@@ -15,10 +36,9 @@ def pos_tests() -> None:
 	expectedPosition = (4,4)
 	roots = positioning.calculate_roots(len(radii), radii, xCoords, yCoords)
 	print(positioning.estimate_position(roots))
-	pass
 
 def main():
-	pos_tests()
+	friis_tests()
 
 if __name__ == "__main__":
 	main()
