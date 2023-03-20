35218ba2d45d7fd086baa68cd137d5f35af72afc
tinpott <77856809+tinpott@users.noreply.github.com>
2023-03-20 18:41:30 -0400

    More clean-up

--- friis.py
+++ friis.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import numpy
 import math
 import sys
 
--- test.py
+++ test.py
@@ -3,36 +3,40 @@
 import friis
 import sys
 import positioning
+import numpy
 import util
 
-def friis_tests(obj_pos: tuple[float, float], wavelength: float, obj_power: float, obj_gain: float, op_x_coords: tuple[float, ...], op_y_coords: tuple[float, ...], op_gains: tuple[float, ...], seed: int | float | None = None) -> None:
+def friis_tests(pos_tx: tuple[float, float], wavelength: float, p_tx: float, g_tx: float, x_coords_rx: tuple[float, ...], y_coords_rx: tuple[float, ...], gains_rx: tuple[float, ...], seed: int | float | None = None) -> None:
 	"""
 	TODO: Add function description
 
 	Args:
-		obj_pos (tuple[float, float]): Object position in 2D coordinate system (m, m)
-		wavelength (float): Wavelength of signal (m)
-		obj_power (float): Signal power of SDR on tracked object (dBm)
-		obj_gain (float): Antenna gain of tracked object (dBi)
-		op_x_coords (tuple[float, ...]): x-coordinate values representing positions of observation points ([m, ...])
-		op_y_coords (tuple[float, ...]): y-coordinate values representing positions of observation points ([m, ...])
-		op_gains (tuple[float, ...]): List of antenna gains of observation points ([dBi, ...])
+		pos_tx (tuple[float, float]): Position of transmitter ([m, m])
+		wavelength (float): Wavelength of transmitted signal (m)
+		p_tx (float): Signal power of transmitter (dBm)
+		g_tx (float): Antenna gain of tracked object (dBi)
+		x_coords_rx (tuple[float, ...]): Receivers' x-coordinate values in 2D plane ([m, ...])
+		y_coords_rx (tuple[float, ...]): Receivers' y-coordinate values in 2D plane ([m, ...])
+		gains_rx (tuple[float, ...]): List of receivers' antenna gains ([dBi, ...])
 		seed (int | float | None): Optional parameter. Seed value for path loss randomness to simulate non-ideal conditions
 
 	Returns
 		TODO: Add return description
-	"""
-	if len(obj_pos) != 2:
-		raise Exception(f"Object position must be a 2-float tuple. Invalid position: {obj_pos}")
-	xc, yc = obj_pos[0], obj_pos[1]
 
-	NUM = len(op_x_coords)
-	if NUM != len(op_y_coords) or NUM != len(op_gains):
+	Raises:
+		TODO: Add exception descriptions
+	"""
+	if len(pos_tx) != 2:
+		raise Exception(f"Object position must be a 2-float tuple. Invalid position: {pos_tx}")
+	xc, yc = pos_tx[0], pos_tx[1]
+
+	NUM = len(x_coords_rx)
+	if NUM != len(y_coords_rx) or NUM != len(gains_rx):
 		raise Exception(\
 			"Observation points' tuples must match in length.\n" + \
-				f"\tOP x-coords ({len(op_x_coords)}): {op_x_coords}\n" + \
-				f"\tOP y-coords ({len(op_y_coords)}): {op_y_coords}\n" + \
-				f"\tOP gains ({len(op_gains)}): {op_gains}" \
+				f"\tOP x-coords ({len(x_coords_rx)}): {x_coords_rx}\n" + \
+				f"\tOP y-coords ({len(y_coords_rx)}): {y_coords_rx}\n" + \
+				f"\tOP gains ({len(gains_rx)}): {gains_rx}" \
 		)
 
 	if not (type(seed) is int or type(seed) is float or seed is None):
@@ -40,20 +44,26 @@ def friis_tests(obj_pos: tuple[float, float], wavelength: float, obj_power: floa
 
 	# Get exact distances between tracked object and observation points
 	distances = list()
-	for x, y in zip(op_x_coords, op_y_coords):
+	for x, y in zip(x_coords_rx, y_coords_rx):
 		distances.append(util.pythagorean_theorem((x,y), (xc, yc)))
+
+	# Get received signal powers
 	powers_rx = list()
 	for i in range(len(distances)):
-		powers_rx.append(friis.standard_form(obj_power, obj_gain, op_gains[i], wavelength, distances[i]))
+		p_rx = friis.standard_form(p_tx, g_tx, gains_rx[i], wavelength, distances[i])
+		if seed is not None:
+			p_rx += numpy.random.normal(0, seed)
+		powers_rx.append(p_rx)
 
+	# Get estimated distances
 	calculated_distances = list()
 	for i in range(len(powers_rx)):
-		calculated_distances.append(friis.distance_form(powers_rx[i], obj_power, op_gains[i], obj_gain, wavelength))
-	print(calculated_distances)
+		calculated_distances.append(friis.distance_form(powers_rx[i], p_tx, gains_rx[i], g_tx, wavelength))
 
-	roots = positioning.calculate_roots(NUM, tuple(calculated_distances), op_x_coords, op_y_coords)
-	print((xc, yc))
-	print(positioning.estimate_position(roots))
+	# Estimate position based on estimated distances
+	roots = positioning.calculate_roots(NUM, tuple(calculated_distances), x_coords_rx, y_coords_rx)
+	print(f"Exact position:     {pos_tx}")
+	print(f"Estimated position: {positioning.estimate_position(roots)}")
 
 def pos_tests() -> None:
 	"""
@@ -73,7 +83,7 @@ def main():
 	y = ( 0.00, 8.00,  5.00 )
 	g = ( 0.00, 0.00,  0.00 )
 	try:
-		friis_tests(obj_pos=(4,4), wavelength=0.1, obj_power=0, obj_gain=0, op_x_coords=x, op_y_coords=y, op_gains=g, seed=2)
+		friis_tests(pos_tx=(4,4), wavelength=0.1, p_tx=0, g_tx=0, x_coords_rx=x, y_coords_rx=y, gains_rx=g, seed=2)
 		#pos_tests()
 	except Exception as e:
 		print(e, file=sys.stderr)
